81dd05e61a6569ff55bebcf071382910d8486882
summersky <summer.sky.2014@qq.com>
2019-05-14 23:40:59 +0800

    feat: 新增请求响应相关的type和interface

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -8,6 +8,18 @@ declare namespace BSL {
   type Env = 'development' | 'production' | 'prerelease';
   type RequestStatus = 'start' | 'end' | undefined;
   type OnClick<T> = (e: React.MouseEvent<T>, ...args: any[]) => void;
+  /** 响应码 */
+  type ResponseCode = (
+    /** 请求成功，对应complete状态 */
+    200 |
+    /** 数据为空，对应empty状态 */
+    404 |
+    /** 请求超时, 对应timeout状态 */
+    408 |
+    /** 请求失败，对应fail状态 */
+    500
+  );
+
   interface PageProps<Match>  {
     match: RouteComponentProps<Match>['match'];
     location: Location;
@@ -20,6 +32,13 @@ declare namespace BSL {
     id?: string;
     /** 组件根元素的样式 */
     style?: React.CSSProperties;
+  }
+
+  /** 请求的响应结构 */
+  interface RequestResponse {
+    data: any;
+    code: ResponseCode;
+    msg: string;
   }
 }
 
