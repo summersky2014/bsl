1f84ddd81a9291a7b6e54a61ed121dbacbef2bc9
summersky2014 <lacie.vessarius@gmail.com>
2019-11-06 18:58:55 +0800

    fix(components/StateView): 修复错误的引用scss文件

--- components/StateView/Fail/index.tsx
+++ components/StateView/Fail/index.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
-import Icon from '../../Icon';
+import * as classNames from 'classnames';
+import { css } from 'aphrodite/no-important';
+import styles from '../Empty/style';
+
 import variable from '../../../utils/variable';
-import '../Empty/index.scss';
+import Icon from '../../Icon';
 
 const emptySvg = variable.svgRootPath + require('../../../assets/empty.svg').id;
 const prefixCls = 'bsl-stateview-empty';
 
 function Fail() {
   return (
-    <div className={prefixCls}>
+    <div className={classNames(css(styles.root), prefixCls)}>
       <Icon src={emptySvg} />
-      <div className={`${prefixCls}-label`}>抱歉，服务器出错了</div>
-      <div className={`${prefixCls}-label`}>轻触屏幕重试</div>
+      <div className={classNames(css(styles.label), `${prefixCls}-label`)}>抱歉，服务器出错了</div>
+      <div className={classNames(css(styles.label), `${prefixCls}-label`)}>轻触屏幕重试</div>
     </div>
   );
 }
