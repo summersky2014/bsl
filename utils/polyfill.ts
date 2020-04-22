c802fcc50b26e2f261470919d05a122011110f64
summersky2014 <lacie.vessarius@gmail.com>
2020-04-22 09:05:38 +0800

    fix(utils/polyfill): 兼容不支持toBlob的设备

--- utils/polyfill.ts
+++ utils/polyfill.ts
@@ -1,5 +1,28 @@
 import 'core-js/es6/map';
-import 'core-js/es6/set';
+import 'core-js/es6/map';
 import 'core-js/es6/promise';
+import 'core-js/es6/promise';
+import 'core-js/es6/set';
+import 'core-js/es6/set';
 
 window.URLSearchParams = require('@ungap/url-search-params').default || require('@ungap/url-search-params') as URLSearchParams;
+
+if (!HTMLCanvasElement.prototype.toBlob) {
+  Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+    value: function (callback: any, type: any, quality: any) {
+      // eslint-disable-next-line @typescript-eslint/no-this-alias
+      const canvas = this;
+      setTimeout(function() {
+        const binStr = atob( canvas.toDataURL(type, quality).split(',')[1] );
+        const len = binStr.length;
+        const arr = new Uint8Array(len);
+
+        for (let i = 0; i < len; i++) {
+          arr[i] = binStr.charCodeAt(i);
+        }
+
+        callback(new Blob([arr], { type: type || 'image/png' }));
+      });
+    }
+  });
+}
